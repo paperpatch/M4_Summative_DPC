f0e2cf6752875f45c8589536bd5e3b02b05a2843
paperpatch <patchen21@gmail.com>
2022-07-12 16:45:15 -0400

    initial commit

---
+++ src/main/java/com/dpc/M4_Summative_DPC/M4SummativeDpcApplication.java
@@ -0,0 +1,13 @@
+package com.dpc.M4_Summative_DPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4SummativeDpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4SummativeDpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpc/M4_Summative_DPC/M4SummativeDpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpc.M4_Summative_DPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4SummativeDpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
