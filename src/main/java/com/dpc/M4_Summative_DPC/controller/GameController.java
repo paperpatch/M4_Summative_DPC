1af449148de29590a3538e56735bb79a3e191cf4
paperpatch <patchen21@gmail.com>
2022-07-13 11:32:26 -0400

    updated games controller. work in progress

--- src/main/java/com/dpc/M4_Summative_DPC/controller/GameController.java
+++ src/main/java/com/dpc/M4_Summative_DPC/controller/GameController.java
@@ -8,20 +8,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class GameController {
     @Autowired
-    GameRepository repo;
+    GameRepository gameRepository;
 
     @GetMapping("/games")
-    public List<Game> getGames() {
-        return repo.findAll();
+    @ResponseStatus(HttpStatus.OK)
+    public List<Game> getGames(@RequestParam(required=false) String title, @RequestParam(required = false) String esrbRating, @RequestParam(required = false) String studio) {
+        List<Game> returnList = gameRepository.findAll();
+        if (title != null) {
+            returnList = returnList.stream()
+                    .filter(g -> g.getTitle().contains(title))
+                    .collect(Collectors.toList());
+        }
+
+        if (esrbRating != null) {
+            returnList = returnList.stream()
+                    .filter(g -> g.getEsrbRating().contains(esrbRating))
+                    .collect(Collectors.toList());
+        }
+
+        if (studio != null) {
+            returnList = returnList.stream()
+                    .filter(g -> g.getEsrbRating().contains(studio))
+                    .collect(Collectors.toList());
+        }
+
+        return returnList;
     }
 
     @GetMapping("/games/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Game getGameById(@PathVariable int id) {
-        Optional<Game> returnVal = repo.findById(id);
+        Optional<Game> returnVal = gameRepository.findById(id);
         if (returnVal.isPresent()) {
             return returnVal.get();
         } else {
@@ -29,21 +51,32 @@ public class GameController {
         }
     }
 
+//    @GetMapping("/games/{title}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public Optional<Game> getGameByTitle(@PathVariable String title) {
+//        Optional<Game> returnVal = gameRepository.findByTitle(title);
+//        if (returnVal.isPresent()) {
+//            return returnVal.get();
+//        } else {
+//            return null;
+//        }
+//    }
+
     @PostMapping("/games")
     @ResponseStatus(HttpStatus.CREATED)
-    public Game addGame(@RequestBody Game artist) {
-        return repo.save(artist);
+    public Game addGame(@RequestBody Game game) {
+        return gameRepository.save(game);
     }
 
     @PutMapping("/games")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateGame(@RequestBody Game artist) {
-        repo.save(artist);
+    public void updateGame(@RequestBody Game game) {
+        gameRepository.save(game);
     }
 
     @DeleteMapping("/games/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteGame(@PathVariable int id) {
-        repo.deleteById(id);
+        gameRepository.deleteById(id);
     }
 }
