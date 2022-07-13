76fc7e06302b8d58b94f38597710a541a3ca6b46
paperpatch <patchen21@gmail.com>
2022-07-13 10:33:31 -0400

    added price and quantity. restructured model

--- src/main/java/com/dpc/M4_Summative_DPC/controller/GameController.java
+++ src/main/java/com/dpc/M4_Summative_DPC/controller/GameController.java
@@ -1,12 +1,11 @@
 package com.dpc.M4_Summative_DPC.controller;
 
-import com.dpc.M4_Summative_DPC.model.Game;
+import com.dpc.M4_Summative_DPC.models.Game;
 import com.dpc.M4_Summative_DPC.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
