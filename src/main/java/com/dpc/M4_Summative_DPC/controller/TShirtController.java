e12f126868b6c6c3bcaafef6a00e7ff959055a94
chaudeveloper <ms_michanguyen@yahoo.com>
2022-07-13 19:08:44 -0400

    complete CRUD for T-Shirt

--- src/main/java/com/dpc/M4_Summative_DPC/controller/TShirtController.java
+++ src/main/java/com/dpc/M4_Summative_DPC/controller/TShirtController.java
@@ -1,7 +1,10 @@
 package com.dpc.M4_Summative_DPC.controller;
 
-import com.dpc.M4_Summative_DPC.models.TShirt;
+
+
 import com.dpc.M4_Summative_DPC.repository.TShirtRepository;
+import com.dpc.M4_Summative_DPC.service.ServiceLayer;
+import com.dpc.M4_Summative_DPC.viewmodel.TShirtViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,35 +15,49 @@ import java.util.Optional;
 @RestController
 public class TShirtController {
     @Autowired
-    TShirtRepository tShirtRepository;
+    ServiceLayer serviceLayer;
 
     @PostMapping("/tshirt")
     @ResponseStatus(HttpStatus.CREATED)
-    public TShirt addTShirt(@RequestBody TShirt tshirt) {return tShirtRepository.save(tshirt);}
+    public TShirtViewModel addTShirt(@RequestBody TShirtViewModel tShirtViewModel) {
+        return serviceLayer.saveTShirt(tShirtViewModel);
+    }
 
     @GetMapping("/tshirt")
     @ResponseStatus(HttpStatus.OK)
-    public List<TShirt> getAllTShirt(@RequestParam(required=false) String color,@RequestParam(required=false) String size) {
-        if(color !=null){
-            return tShirtRepository.findByColor(color);
-        }
-        if(size !=null){
-            return tShirtRepository.findBySize(size);
-        }
-        return tShirtRepository.findAll();
+    public List<TShirtViewModel> getAllTShirt() {
+       return serviceLayer.findAllTshirt();
+    }
+
+    @GetMapping("/tshirt/color/{color}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TShirtViewModel> getAllTShirtByColor(@PathVariable("color") String color) {
+        return serviceLayer.findAllTshirtByColor(color);
+    }
+
+    @GetMapping("/tshirt/size/{size}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TShirtViewModel> getAllTShirtBySize(@PathVariable("size")String size) {
+        return serviceLayer.findAllTshirtBySize(size);
     }
 
     @GetMapping("/tshirt/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TShirt findTShirtById(@PathVariable int id){
-        Optional<TShirt> tShirt = tShirtRepository.findById(id);
-
-        if(tShirt.isPresent() == false){
-            throw new IllegalArgumentException("There's no T-Shirt with this ID");
-        }
-        return tShirt.get();
+    public TShirtViewModel findTShirtById(@PathVariable int id){
+       return serviceLayer.findATShirtById(id);
     }
 
+    @PutMapping("/tshirt")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateTShirt(@RequestBody TShirtViewModel tShirtViewModel) {
+        serviceLayer.updateTShirt(tShirtViewModel);
+    }
+
+    @DeleteMapping("/tshirt/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTShirt(@PathVariable int id) {
+        serviceLayer.removeTShirt(id);
+    }
 
 
 
