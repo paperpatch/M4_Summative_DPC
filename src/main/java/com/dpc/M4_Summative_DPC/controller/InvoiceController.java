07811a348dc16b22b5195d96c4524989f5a0170b
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-14 06:39:54 -0700

    Invoice controller developed with standard CRUD operations.

--- src/main/java/com/dpc/M4_Summative_DPC/controller/InvoiceController.java
+++ src/main/java/com/dpc/M4_Summative_DPC/controller/InvoiceController.java
@@ -1,2 +1,60 @@
-package com.dpc.M4_Summative_DPC.controller;public class InvoiceController {
+package com.dpc.M4_Summative_DPC.controller;
+
+import com.dpc.M4_Summative_DPC.models.Invoice;
+import com.dpc.M4_Summative_DPC.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class InvoiceController {
+    @Autowired
+    InvoiceRepository invoiceRepository;
+
+    // Create a invoice
+    @RequestMapping(value = "/invoice", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Invoice createInvoice(@RequestBody Invoice invoice) {
+        
+        return invoiceRepository.save(invoice);
+    }
+    //    Get all invoice
+    @RequestMapping(value = "/invoice", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Invoice> getAllInvoice() {
+      
+        return invoiceRepository.findAll();
+    }
+    //    Get invoice by id
+    @RequestMapping(value = "/invoice/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Invoice getInvoiceById(@PathVariable int id) {
+        Optional<Invoice> invoice = invoiceRepository.findById(id);
+        if (!invoice.isPresent()) {
+            throw new IllegalArgumentException("Invalid id, enter the correct id.");
+        }
+        return invoice.get();
+    }
+    //    Update a invoice
+    @RequestMapping(value = "invoice/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateAnInvoice(@PathVariable int id, @RequestBody Invoice invoice) {
+        if (invoice.getInvoiceId() == null) {
+            invoice.setInvoiceId(id);
+        } else if (invoice.getInvoiceId() != id) {
+
+            throw new IllegalArgumentException("Invalid id, enter the correct id.");
+        }
+        invoiceRepository.save(invoice);
+    }
+    //    Delete invoice
+    @RequestMapping(value = "/invoice/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroyInvoice(@PathVariable int id) {
+        invoiceRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/dpc/M4_Summative_DPC/models/Invoice.java
+++ src/main/java/com/dpc/M4_Summative_DPC/models/Invoice.java
@@ -1,2 +1,205 @@
-package com.dpc.M4_Summative_DPC.models;public class Invoice {
+package com.dpc.M4_Summative_DPC.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "invoice")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "invoice_id")
+    private Integer invoiceId;
+    private String name;
+    private String street;
+    private String city;
+    @NotEmpty
+    private String state;
+    private String Zipcode;
+    @Column(name= "item_type")
+    private String itemType;
+    @Column(name= "item_id")
+    private Integer itemId;
+    @Column(name= "unit_price")
+    private Double unitPrice;
+    @NotEmpty
+    private Integer quantity;
+    private Double subtotal;
+    @Column(name= "sale_tax_rate")
+    private Double saleTaxRate;
+    @Column(name= "processing_fee")
+    private Double processingFee;
+    private Double total;
+
+    public Invoice(Integer invoiceId, String name, String street, String city, String state, String zipcode, String itemType, Integer itemId, Double unitPrice, Integer quantity, Double subtotal, Double saleTaxRate, Double processingFee, Double total) {
+        this.invoiceId = invoiceId;
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        Zipcode = zipcode;
+        this.itemType = itemType;
+        this.itemId = itemId;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.subtotal = subtotal;
+        this.saleTaxRate = saleTaxRate;
+        this.processingFee = processingFee;
+        this.total = total;
+    }
+
+    public Invoice() {
+    }
+
+    public Integer getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Integer invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return Zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        Zipcode = zipcode;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getSaleTaxRate() {
+        return saleTaxRate;
+    }
+
+    public void setSaleTaxRate(Double saleTaxRate) {
+        this.saleTaxRate = saleTaxRate;
+    }
+
+    public Double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(Double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Invoice invoice = (Invoice) o;
+        return Objects.equals(invoiceId, invoice.invoiceId) && Objects.equals(name, invoice.name) && Objects.equals(street, invoice.street) && Objects.equals(city, invoice.city) && Objects.equals(state, invoice.state) && Objects.equals(Zipcode, invoice.Zipcode) && Objects.equals(itemType, invoice.itemType) && Objects.equals(itemId, invoice.itemId) && Objects.equals(unitPrice, invoice.unitPrice) && Objects.equals(quantity, invoice.quantity) && Objects.equals(subtotal, invoice.subtotal) && Objects.equals(saleTaxRate, invoice.saleTaxRate) && Objects.equals(processingFee, invoice.processingFee) && Objects.equals(total, invoice.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(invoiceId, name, street, city, state, Zipcode, itemType, itemId, unitPrice, quantity, subtotal, saleTaxRate, processingFee, total);
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoiceId=" + invoiceId +
+                ", name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", Zipcode='" + Zipcode + '\'' +
+                ", itemType='" + itemType + '\'' +
+                ", itemId=" + itemId +
+                ", unitPrice=" + unitPrice +
+                ", quantity=" + quantity +
+                ", subtotal=" + subtotal +
+                ", saleTaxRate=" + saleTaxRate +
+                ", processingFee=" + processingFee +
+                ", total=" + total +
+                '}';
+    }
 }
