da44a0514848a74cb5bf5cf110b5264970241a10
paperpatch <patchen21@gmail.com>
2022-07-14 11:22:23 -0400

    fixed processing fee and sales tax table generation for application to start server

--- src/main/java/com/dpc/M4_Summative_DPC/models/ProcessingFee.java
+++ src/main/java/com/dpc/M4_Summative_DPC/models/ProcessingFee.java
@@ -3,25 +3,41 @@ package com.dpc.M4_Summative_DPC.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name = "processing_fee", indexes = {@Index(name = "ix_product_type_fee", columnList="product_type")})
+@Table(name = "processing_fee")
 public class ProcessingFee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotEmpty
     @Column(name = "product_type")
     private String productType;
-
     private double fee;
 
     public ProcessingFee() {
     }
 
+    public ProcessingFee(int id, String productType, double fee) {
+        this.id = id;
+        this.productType = productType;
+        this.fee = fee;
+    }
+
     public ProcessingFee(String productType, double fee) {
         this.productType = productType;
         this.fee = fee;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getProductType() {
@@ -45,18 +61,19 @@ public class ProcessingFee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ProcessingFee that = (ProcessingFee) o;
-        return Double.compare(that.fee, fee) == 0 && Objects.equals(productType, that.productType);
+        return id == that.id && Double.compare(that.fee, fee) == 0 && Objects.equals(productType, that.productType);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(productType, fee);
+        return Objects.hash(id, productType, fee);
     }
 
     @Override
     public String toString() {
         return "ProcessingFee{" +
-                "productType='" + productType + '\'' +
+                "id=" + id +
+                ", productType='" + productType + '\'' +
                 ", fee=" + fee +
                 '}';
     }
