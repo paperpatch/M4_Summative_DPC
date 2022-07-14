4d8cd4b1349d018a3960c11a6ed2e19223ea6578
paperpatch <patchen21@gmail.com>
2022-07-14 12:41:44 -0400

    added fake data for seeding

--- src/main/java/com/dpc/M4_Summative_DPC/controller/SeedController.java
+++ src/main/java/com/dpc/M4_Summative_DPC/controller/SeedController.java
@@ -1,5 +1,8 @@
 package com.dpc.M4_Summative_DPC.controller;
 
+import com.dpc.M4_Summative_DPC.models.Console;
+import com.dpc.M4_Summative_DPC.models.Game;
+import com.dpc.M4_Summative_DPC.models.TShirt;
 import com.dpc.M4_Summative_DPC.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,5 +21,17 @@ public class SeedController {
         serviceLayer.clearDatabase();
         serviceLayer.seedTaxes();
         serviceLayer.seedFees();
+
+        serviceLayer.addGame(new Game("Elden Ring", "M (Mature 17+)", "Elden Ring sees you play as an initially meaningless character in a world of monsters and demigods, all struggling for control over the Lands Between.", 59.95, "FromSoftware Inc.", 1));
+        serviceLayer.addGame(new Game("LEGO Star Wars: The Skywalker Saga", "E (Everyone)", "Lego-themed action-adventure game.", 49.00, "Warner Bros. Interactive Entertainment", 2));
+        serviceLayer.addGame(new Game("Among Us", "E (Everyone)", "Online multiplayer social deduction game.", 4.99, "InnerSloth LLC", 5));
+
+        serviceLayer.addConsole(new Console("Playstation 5", "Sony", "1 TB", "AMD Zen 2-CPU", 549.99, 20));
+        serviceLayer.addConsole(new Console("Xbox Series X", "Microsoft", "1 TB", "AMD Zen 2-CPU", 499.99, 10));
+        serviceLayer.addConsole(new Console("Nintendo Switch", "Nintendo", "32 GB", "Quad-core ARM Cortex", 299.99, 15));
+
+        serviceLayer.saveTShirt(new TShirt("small", "red", "small red shirt", 9.99, 14));
+        serviceLayer.saveTShirt(new TShirt("medium", "blue", "medium sized blue shirt", 9.99, 30));
+        serviceLayer.saveTShirt(new TShirt("large", "green", "large sized green shirt", 9.99, 21));
     }
 }
