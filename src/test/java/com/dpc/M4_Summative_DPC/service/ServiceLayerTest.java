a4fc934c4e0b65cac4beb4c59cf4d7ce0a76a05a
paperpatch <patchen21@gmail.com>
2022-07-15 15:24:09 -0400

    set up mock Repositories

--- src/test/java/com/dpc/M4_Summative_DPC/service/ServiceLayerTest.java
+++ src/test/java/com/dpc/M4_Summative_DPC/service/ServiceLayerTest.java
@@ -1,7 +1,6 @@
 package com.dpc.M4_Summative_DPC.service;
 
-import com.dpc.M4_Summative_DPC.models.Game;
-import com.dpc.M4_Summative_DPC.models.TShirt;
+import com.dpc.M4_Summative_DPC.models.*;
 import com.dpc.M4_Summative_DPC.repository.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -82,13 +81,65 @@ public class ServiceLayerTest {
         game2.setQuantity(50);
     }
 
-    private void setUpConsoleRepositoryMock() {}
+    private void setUpConsoleRepositoryMock() {
+        consoleRepository = mock(ConsoleRepository.class);
+        Console console = new Console();
+        console.setConsoleId(1);
+        console.setModel("Playstation 5");
+        console.setManufacturer("Sony");
+        console.setMemoryAmount("1 TB");
+        console.setProcessor("AMD Zen 2-CPU");
+        console.setPrice(549.99);
+        console.setQuantity(20);
 
-    private void setUpInvoiceRepositoryMock() {}
+        Console console2 = new Console();
+        console2.setConsoleId(1);
+        console2.setModel("Xbox Series X");
+        console2.setManufacturer("Microsoft");
+        console2.setMemoryAmount("1 TB");
+        console2.setProcessor("AMD Zen 2-CPU");
+        console2.setPrice(499.99);
+        console2.setQuantity(10);
+    }
 
-    private void setUpSalesTaxRateRepositoryMock() {}
+    private void setUpInvoiceRepositoryMock() {
+        invoiceRepository = mock(InvoiceRepository.class);
+        Invoice invoice = new Invoice();
+        invoice.setId(1);
+        invoice.setStreet("123 fake street");
+        invoice.setCity("Fake City");
+        invoice.setState("CT");
+        invoice.setZipCode("00000");
+        invoice.setItemType("games");
+        invoice.setItemId(1);
+        invoice.setQuantity(2);
 
-    private void setUpProcessingFeeRepositoryMock() {}
+        gameRepository = mock(GameRepository.class);
+        Game game = new Game();
+        game.setId(1);
+        game.setTitle("Elden Ring");
+        game.setEsrbRating("M (Mature 17+)");
+        game.setDescription("Elden Ring sees you play as an initially meaningless character in a world of monsters and demigods, all struggling for control over the Lands Between.");
+        game.setPrice(59.95);
+        game.setStudio("FromSoftware Inc.");
+        game.setQuantity(50);
+    }
+
+    private void setUpSalesTaxRateRepositoryMock() {
+        salesTaxRateRepository = mock(SalesTaxRateRepository.class);
+        SalesTaxRate tax = new SalesTaxRate();
+        tax.setId(1);
+        tax.setState("CT");
+        tax.setRate(0.03);
+    }
+
+    private void setUpProcessingFeeRepositoryMock() {
+        processingFeeRepository = mock(ProcessingFeeRepository.class);
+        ProcessingFee fee = new ProcessingFee();
+        fee.setId(1);
+        fee.setProductType("Games");
+        fee.setFee(1.49);
+    }
 
     @Test
     public void shouldFindTShirts() {
